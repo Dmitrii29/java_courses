6c779ec25e1fcb1496a1cd464cbcb950cc2d8fcf
Dmitrii29 <malivanovdmitrii@gmail.com>
2017-04-03 22:31:00 +0600

    Реализована проверка и выполнение предусловий

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -49,4 +49,15 @@ public class GroupHelper extends HelperBase{
   public void submitGroupModification() {
     click(By.xpath(".//*[@id='content']/form/input[3]"));
   }
+
+  public void createGroup(GroupData group) {
+    initGroupCreation();
+    fillGroupForm(group);
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+  public boolean isThereAGroup() {
+    return isElementPresent(By.name("selected[]"));
+  }
 }
